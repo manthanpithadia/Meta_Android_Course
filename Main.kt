c07ae09dbf12202a57cd558466c6e775c7c2d8ea
Developer1449 <m.a1449pithadia@gmail.com>
2024-04-19 16:06:20 -0400

    week1 completed

--- Main.kt
+++ Main.kt
@@ -1,8 +1,29 @@
+/**
+ * Numbers:
+ * to write , in between the number for large values we use _ to separate the numbers
+ * ex : var i = 97798_44_4L
+ *
+ * a++ : postfix will be executed after the equation is evaluated.
+ * var a = 5
+ * val eq = 6 + a++ // the result will be 11 and then a will be = 6
+ * valnewEq = 6+a // the result will be = 12
+ */
 
 fun main() {
-    println("Total Score: ${printScore().first}, Total Average: ${printScore().second}")
+    val loopPractice = LoopPractice()
+    loopPractice.triangleShape4()
 }
 
+
+
+
+fun learString(){
+//    string with multiple line: use """
+    val multiLineString = """Hello This is a multi line text
+   This just a demo text
+        """
+    print(multiLineString)
+}
 
 fun printScore(): Pair<Int,Int>{
     val scoreLevel1 = 79
